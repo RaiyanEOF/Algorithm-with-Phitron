ff1b08b2d92b193dabe8ab1f4978caea3f9b00c9
Shah Musfique Raiyan Ashrafi <talking2myselfsmra2003@gmail.com>
2025-08-22 19:27:32 +0600

    Update COUNT_APARTMENTS.cpp

--- Assignment 01/COUNT_APARTMENTS.cpp
+++ Assignment 01/COUNT_APARTMENTS.cpp
@@ -1,30 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 int N, M;
-vector<string> grid;
-vector<vector<bool>> vis;
-int dx[] = {-1, 1, 0, 0};  
-int dy[] = {0, 0, -1, 1};  
-bool isValid(int x, int y){
-    return x>=0 && x<N && y>=0 && y<M && grid[x][y] == '.' && !vis[x][y];
+char grid[1005][1005];
+bool vis [1005][1005];
+vector<pair<int,int>> d = {{-1,0},{1,0},{0,-1},{0,1}};  
+bool isValid(int i, int j){
+    return i>=0 && i<N && j>=0 && j<M && grid[i][j] == '.' && !vis[i][j];
 }
-void dfs(int x, int y){
-    vis[x][y] = true;
+void dfs(int si, int sj){
+    vis[si][sj] = true;
     for(int i = 0; i < 4; i++){
-        int newX = x + dx[i];
-        int newY = y + dy[i];
-        if(isValid(newX, newY)){
-            dfs(newX, newY);
+        int ci = si + d[i].first;
+        int cj = sj + d[i].second;
+        if(isValid(ci, cj)){
+            dfs(ci,cj);
         }
     }
 }
 int main(){
     cin >> N >> M;
-    grid.resize(N);
-    vis.resize(N, vector<bool>(M, false));
     for(int i = 0; i < N; i++){
         cin >> grid[i];
     }
+    memset(vis,false,sizeof(false));
     int Count = 0;
     for(int i = 0; i < N; i++){
         for(int j = 0; j < M; j++){
